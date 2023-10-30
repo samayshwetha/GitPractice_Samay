ed502209346a9508ab653b9758b039766ab6e568
Shwetha Nadagi <Snadagi@nextgen.com>
2023-10-30 19:56:52 +0530

    Added new class Demo1

---
+++ src/test/java/practicegit/Demo1.java
@@ -0,0 +1,18 @@
+package practicegit;
+
+public class Demo1 {
+
+	
+	public static void main(String[] args) {
+		
+		   System.out.println("Hi From JAVA");
+		   System.out.println("Hi From C++");
+
+		}
+		
+		public void Newmethod1()
+		{
+			System.out.println("Hi From NewMethod1");
+		}
+	}
+
